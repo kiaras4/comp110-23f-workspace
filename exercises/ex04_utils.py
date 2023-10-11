1244530a38d6b01f293935ada10f2ef185f13e77
Kiara Smith <kcsmith4@unc.edu>
2023-10-10 22:34:23 -0400

    progress on ex04

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -4,19 +4,24 @@ __author__ = "730675328"
 
 
 def all(list1: list[int], num: int) -> bool:
-    "Tests if the list is entirely comprised of the given integer"
+    """Tests if the list is entirely comprised of the given integer."""
     idx = 0
     # loops through all indices
     while idx < len(list1):
+        if len(list1) == 0:
+            return False
         if num != list1[idx]:
             return False
         elif num == list1[idx] and idx < len(list1) - 1:
             idx += 1
         else:
             return True
-    
+        
+    return False
+
+
 def max(input: list[int]) -> int:
-    """Determines max of given list"""
+    """Determines max of given list."""
     if len(input) == 0:
         raise ValueError("max() arg is an empty List")
     
@@ -35,10 +40,16 @@ def max(input: list[int]) -> int:
 
     return max1
 
-def is_equal(list1: list, list2: list):
-    """Determines if given lists are equal"""
-    idx = 0
 
+def is_equal(list1: list[int], list2: list[int]) -> bool:
+    """Determines if given lists are equal."""
+    idx = 0
+    if len(list1) == 0 and len(list2) == 0:
+        return True
+    elif len(list1) == 0 or len(list2) == 0:
+        return False
+    if len(list1) != len(list2):
+        return False
     # loops through indices
     while idx < len(list1):
         if list1[idx] != list2[idx]:
@@ -47,3 +58,5 @@ def is_equal(list1: list, list2: list):
             idx += 1
         else:
             return True
+    
+    return False
